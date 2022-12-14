616c214e4fb183556cf81293c6e10170663d1edd
20212078-SakaiKazuya <20212078-sakaikazuya@hcs.ac.jp>
2022-12-14 16:29:42 +0900

    できてないけどコミット

--- www/js/login.js
+++ www/js/login.js
@@ -1,15 +1,9 @@
 // This is a JavaScript file
-'use strict'
 
 function login(){
-    // ニフクラ接続
-    var applicationKey = '31f236de7d5148a5cb93c53b52bfdd0fb4469aa6a3f4f0e8a39afa23f917241e';
-    var clientKey = 'b59e281093d74f2fcb80e83a1aaf70d106e73e6059b429e5f39298a9884ae771';
-    var ncmb = new NCMB(applicationKey, clientKey);
-
     // ログイン情報の取得
-    var loginUserName = document.getElementById('username').value;
-    var loginUserPassword = document.getElementById('password').value;
+    var loginUserName = document.getElementById("username").value;
+    var loginUserPassword = document.getElementById("password").value;
     var loginFlg = false;
 
     // 入力チェック
@@ -19,15 +13,17 @@ function login(){
     if(loginUserPassword == ""){
         window.alert('パスワードを入力してください');
     }
-
-    ncmb.User.login(loginUserName, loginUserPassword)
-        .then(function(user) {
+    var user = new ncmb.User({userName:loginUserName, password:loginUserPassword});
+    ncmb.User.login(user)
+        .then(function(data) {
             // ログイン成功時
+            console.log(loginUserName + ' & ' + loginUserPassword);
             console.log('ログインしました');
             loginFlg = true;
         })
         .catch(function(e){
             // ログイン失敗時
+            console.log(loginUserName + ' & ' + loginUserPassword);
             console.log('ログインに失敗しました');
         });
     if(loginFlg){
--- www/js/signup.js
+++ www/js/signup.js
@@ -1,2 +1,25 @@
 // This is a JavaScript file
 
+function signup(){
+    //新規登録画面から値を取得
+    var signupUserName = document.getElementById("username").value;
+    var signupUserMailaddress = document.getElementById("mail").value;
+    var signupUserPassword = document.getElementById("password").value;
+
+    // userインスタンスの生成
+    var newUser = new ncmb.User();
+    // ユーザー名,パスワード,メールアドレスを設定
+    newUser.set("userName", signupUserName)  /*ユーザー名*/
+           .set("password", signupUserPassword) /*パスワード*/
+           .set("mailAddress", signupUserMailaddress)   /*メールアドレス*/
+    // 新規登録処理
+    newUser.signUpByAccount()
+           .then(function(){
+               //登録処理
+               console.log('登録しました');
+           })
+           .catch(function(e){
+               //エラー処理
+               console.log('登録できませんでした');
+           });
+}
