73acdd2e108622fe367648592698cd2b22c12f5e
20212078-SakaiKazuya <20212078-sakaikazuya@hcs.ac.jp>
2022-12-23 10:45:13 +0900

    komitto

--- www/js/album.js
+++ www/js/album.js
@@ -4,6 +4,11 @@
 var applicationKey = '31f236de7d5148a5cb93c53b52bfdd0fb4469aa6a3f4f0e8a39afa23f917241e';
 var clientKey = 'b59e281093d74f2fcb80e83a1aaf70d106e73e6059b429e5f39298a9884ae771';
 var ncmb = new NCMB(applicationKey, clientKey);
+var reader = new FileReader();
+reader.onload = function(e) {
+    var dataUrl = reader.result;
+    document.getElementById("albumlist").src = dataUrl;
+}
 
 function getCurUser() {
     // カレントユーザーの取得
@@ -27,74 +32,59 @@ function getCurUser() {
 // 処理の一時停止
 const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-// 日付の書式設定
-function settingDay(date){
-    var getDate; // 引数からの取り出し
-    var result; // 切り取り後の日付
-    var setDate = [];   // 変換後の日付格納用
-    // 日付の切り取り
-    for(var i = 0; i < date.length; i++){
-        getDate = date[i];
-        result = getDate.slice(0, 10);
-        setDate[i] = result;
-    }
-    return setDate;
-}
-
 // htmlの書き込み
-async function writeHTML(pinName,longitude,latitude,date) {
-    var htmlPinLists = '';  // html表示用
+async function writeHTML(pictureName) {
+    var htmlPictureLists = '';  // html表示用
     // html作成
-    if(pinName == "") {
-        htmlPinLists += '<li class="nopinlist">まだ登録されていません！</li>';
+    if (pictureName == "") {
+        htmlPictureLists += '<li class="nopicturelist">まだ登録されていません！</li>';
     } else {
-        for(var i = 0; i < date.length; i++){
-            htmlPinLists += '<li class="pinlist">ピン:' + pinName[i] + '<br>' + '経度:' + longitude[i] + ' 緯度:' + latitude[i] + '<br>' + '作成日時:' + date[i] + '</li><br>';
-        }
+        // for(var i = 0; i < date.length; i++){   // fix
+        //     htmlPictureLists += '<li class="picturelist">ピン:' + pinName[i] + '<br>' + '経度:' + longitude[i] + ' 緯度:' + latitude[i] + '<br>' + '作成日時:' + date[i] + '</li><br>';
+        // }
+        // デバッグ
+        htmlPictureLists += '通った！';
     }
-    await console.log(htmlPinLists);
-    document.getElementById('albumlist').innerHTML = htmlPinLists;
+    await console.log(htmlPictureLists);
+    document.getElementById('albumlist').innerHTML = htmlPictureLists;
 }
 
-// ピンの一覧取得
-window.onload = async function getPinList() {
+// 写真の一覧取得
+window.onload = async function getPictureList() {
     await wait(1000);    // 1秒停止
     var nowUserName = await getCurUser();
-    var pinLists = [];  // ピンの緯度,経度
-    var pinNames = [];  // ピンの名前
-    var lat = [];   // 緯度
-    var long = [];  // 経度
-    var registDay = []; // 作成日付の取得用
-    var settingRegistDay = [];  // 変換後の日付
+    var pictureNames = [];  // 写真の名前
+    var pictureData = [];   // ファイルストアから取得したデータの格納用
+    var binaryPictureData = []; // バイナリデータ格納用
     console.log(nowUserName);
-    // ユーザーのピンを検索
-    var Pin = ncmb.DataStore("pin");
-    await Pin.equalTo("userName", nowUserName)
-        .order("pinId")
+    // ユーザーの写真を検索(データストア内)
+    var Picture = ncmb.DataStore("picture");
+    await Picture.equalTo("userName", nowUserName)
+        .order("pictureId")
         .fetchAll()
         .then(function (result) {
             console.log('検索結果: ' + JSON.stringify(result));
             console.log('件数: ' + result.length);
             for (var i = 0; i < result.length; i++) {
-                pinLists[i] = result[i].map;
-                pinNames[i] = result[i].pinName;
-                registDay[i] = result[i].createDate;
-                lat[i] = pinLists[i].latitude;
-                long[i] = pinLists[i].longitude;
+                pictureNames[i] = result[i].data;
             }
-            // 日付の切り取り
-            settingRegistDay = settingDay(registDay);
             // デバッグ
-            console.log('pinLists: ' + pinLists);
-            console.log('pinNames: ' + pinNames);
-            console.log('lat: ' + lat);
-            console.log('long: ' + long);
-            console.log('registDay: ' + registDay);
-            console.log('settingRegistDay: ' + settingRegistDay);
+            console.log('pictureNames: ' + pictureNames);
         })
         .catch(function (e) {
-            pinListError();
+            pinListError(); // fix
         });
+    // データの取得
+    for (var i = 0; i < pictureNames.length; i++) {
+        ncmb.File.download(pictureNames[i], "jpeg")
+            .then(function (fileData) {
+                reader.readAsDataURL(fileData);
+            })
+            .catch(function (e) {
+                console.log('エラーが発生しました！');
+            });
+    }
+    await console.log('binaryPictureData: ' + binaryPictureData);
     // htmlの書き換え
-    writeHTML(pinNames, long, lat, settingRegistDay);
+    writeHTML(pictureNames);
 }
