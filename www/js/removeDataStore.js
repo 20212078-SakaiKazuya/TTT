3435ada2647b815f20ac20770b5a5c8ab465eeb7
20212078-SakaiKazuya <20212078-sakaikazuya@hcs.ac.jp>
2023-01-19 10:20:11 +0900

    k

--- www/js/removeDataStore.js
+++ www/js/removeDataStore.js
@@ -1,9 +1,9 @@
 // This is a JavaScript file
 
 // ニフクラ接続
-// var applicationKey = '31f236de7d5148a5cb93c53b52bfdd0fb4469aa6a3f4f0e8a39afa23f917241e';
-// var clientKey = 'b59e281093d74f2fcb80e83a1aaf70d106e73e6059b429e5f39298a9884ae771';
-// var ncmb = new NCMB(applicationKey, clientKey);
+var applicationKey = '31f236de7d5148a5cb93c53b52bfdd0fb4469aa6a3f4f0e8a39afa23f917241e';
+var clientKey = 'b59e281093d74f2fcb80e83a1aaf70d106e73e6059b429e5f39298a9884ae771';
+var ncmb = new NCMB(applicationKey, clientKey);
 
 // function getCurUser() {
 //     // カレントユーザーの取得
@@ -31,6 +31,7 @@
 async function pinRemove(pinId) {
     var pinID = pinId;      // pinID
     // 対象のデータを検索
+    console.log('pinID:' + pinID);
     var Pin = ncmb.DataStore("pin");
     var pin = new Pin();
     await Pin.equalTo("pinID", pinID)
@@ -38,25 +39,23 @@ async function pinRemove(pinId) {
         .then(function (pin) {
             // デバッグ
             console.log('検索結果:' + JSON.stringify(pin));
-            if(pin.length != 0) {
-                pin.delete()
-                    .then(function(result) {
-                        console.log('削除完了');
-                        truePinDelete();
-                    })
-                    .catch(function(err) {
-                        console.log('削除失敗');
-                        falsePinDelete();
-                    });
-            }
+            pin.delete()
+                .then(function (result) {
+                    console.log('削除完了');
+                    truePinDelete();
+                })
+                .catch(function (err) {
+                    console.log('削除失敗');
+                    falsePinDelete();
+                });
         })
-        .catch(function(err) {
+        .catch(function (err) {
             console.log('データが見つかりません');
         });
 }
-     
+
 // 写真の削除
 function pictureDelete(pictureId) {
     var pictureID = pictureId;  // pictureID
-    
+
 }
